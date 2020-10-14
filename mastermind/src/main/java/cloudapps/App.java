60d927f06462216877e0bb635c05fb29ae09376f
david-rojo <davidrojotech@gmail.com>
2020-10-14 02:00:43 +0200

    initial java project commit

---
+++ mastermind/src/main/java/cloudapps/App.java
@@ -0,0 +1,13 @@
+package cloudapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
