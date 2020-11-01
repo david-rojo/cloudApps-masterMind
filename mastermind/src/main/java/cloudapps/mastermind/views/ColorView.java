471de20fe5b507f6729c240d45372c2bee19a37d
david-rojo <davidrojotech@gmail.com>
2020-11-01 11:51:55 +0100

    removed unused code

--- mastermind/src/main/java/cloudapps/mastermind/views/ColorView.java
+++ mastermind/src/main/java/cloudapps/mastermind/views/ColorView.java
@@ -5,8 +5,6 @@ import cloudapps.mastermind.models.Color;
 public class ColorView {
 	
 	public static final char[] INITIALS = {'r', 'b', 'y', 'g', 'o', 'p'};
-	
-	public static final char INVALID_COLOR = 0;
 
 	protected Color color;
 
@@ -29,19 +27,6 @@ public class ColorView {
 			result += character;
 		}
 		return result;
-	}
-	
-	public static char getChar(Color color) {
-		for (int i = 0; i < ColorView.INITIALS.length; i++) {
-			if (Color.values()[i] == color) {
-				return ColorView.INITIALS[i];
-			}
-		}
-		return getInvalidColor();
-	}
-	
-	public static char getInvalidColor() {
-		return INVALID_COLOR;
 	}
 
 }
