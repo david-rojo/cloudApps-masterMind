fb2afdcbeba7cbdf1b5a88068b37b421a78f8926
david-rojo <davidrojotech@gmail.com>
2020-11-01 02:37:35 +0100

    deleted unused class

--- mastermind/src/main/java/cloudapps/mastermind/views/View.java
+++
@@ -1,9 +0,0 @@
-package cloudapps.mastermind.views;
-
-import cloudapps.mastermind.controllers.UseCaseController;
-
-public abstract class View {
-
-	public abstract void interact(UseCaseController controller);
-
-}
